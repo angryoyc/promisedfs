9abc9f871cc87408afa4d87cff3dc210ce3ea4d7
Serg A. Osipov (attava) <serg.osipov@gmail.com>
2015-02-26 17:03:09 +1000

    modified:   promisedfs.js

---
+++ promisedfs.js
@@ -0,0 +1,30 @@
+/** @module Promised FS
+ * @name media
+ * @author Serg A. Osipov
+ * @email serg.osipov@gmail.com
+ * @overview Wrapper for some methods of system fs module with promises
+ */
+
+var fs = require('fs');
+var RSVP = require('rsvp');
+
+exports.exists=function(path){
+	return new RSVP.Promise(function(resolve, reject){
+		fs.exists(path, function(exist){
+			resolve(exist);
+		});
+	});
+};
+
+exports.unlink function(path){
+	return new RSVP.Promise(function(resolve, reject){
+		fs.unlink(path, function(err){
+			if(err){
+				reject(err);
+			}else{
+				resolve(true);
+			};
+		});
+	});
+};
+
